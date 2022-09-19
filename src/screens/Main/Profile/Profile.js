d3cabb697b9d73e7290d6e7ea37c1bc2d3dd76ce
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-19 19:26:18 +0300

    Profile Screen completed

--- src/screens/Main/Profile/Profile.js
+++ src/screens/Main/Profile/Profile.js
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
-
+import styles from "./Profile.style";
+import { MaterialIcons } from "@expo/vector-icons";
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={styles.profileContainer}>
+      <View style={styles.profilImageContainer}>
+        <Image
+          style={styles.profilImage}
+          source={{
+            uri: "https://www.rpetru.com/wp-content/uploads/2022/03/r.petruccillo_dwight-schrute_oil-on-panel_6x6_2022.jpg",
+          }}
+        />
+      </View>
+      <TouchableOpacity style={styles.btnContainer}>
+        <Text style={styles.btnText}>Theme</Text>
+        <MaterialIcons style={styles.icon} name="keyboard-arrow-right" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnContainer}>
+        <Text style={styles.btnText}>Edit Profile</Text>
+        <MaterialIcons style={styles.icon} name="keyboard-arrow-right" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btnContainer}>
+        <Text style={styles.btnText}>Log Out</Text>
+        <MaterialIcons style={styles.icon} name="keyboard-arrow-right" />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Main/Profile/Profile.style.js
+++ src/screens/Main/Profile/Profile.style.js
@@ -1,3 +1,35 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  profileContainer: {
+    flex: 1,
+    justifyContent: "space-evenly",
+  },
+  profilImageContainer: {
+    alignItems: "center",
+  },
+  profilImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    resizeMode: "stretch",
+    marginTop: 30,
+  },
+  btnContainer: {
+    marginRight: 50,
+    backgroundColor: "#a05dcd",
+    padding: 10,
+    borderBottomRightRadius: 30,
+    borderTopRightRadius: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  btnText: {
+    fontSize: 24,
+    color: "#fff",
+  },
+  icon: {
+    fontSize: 26,
+    color: "white",
+  },
+});
