5f4bbdfaf391fd3d73df412e34e0bbcea87dc919
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-20 15:23:01 +0300

    Home page permissions completed

--- src/screens/Main/Home/Home.js
+++ src/screens/Main/Home/Home.js
@@ -1,4 +1,10 @@
-import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  Alert,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import styles from "./Home.style";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -6,13 +12,25 @@ import * as ImagePicker from "expo-image-picker";
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../../../redux/userSlice";
+import { Camera, CameraType } from "expo-camera";
+
 const Home = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
   const [image, setImage] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    setImage(result.uri);
+  };
+  const handleCamera = async () => {
+    let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
@@ -28,6 +46,20 @@ const Home = () => {
     }
   }, [pickImage]);
 
+  useEffect(() => {
+    // kamera izinleri için tetikleriz. Expodadan kameraya erişim izni verilmeli!
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, [handleCamera]);
+
+  if (hasPermission === null) {
+  }
+  if (hasPermission === false) {
+    Alert.alert("Warning", "No Access To Camera");
+  }
+
   return (
     <ImageBackground
       source={require("../../../assets/backgr.jpg")}
@@ -38,7 +70,7 @@ const Home = () => {
         <MaterialIcons name="photo-library" size={50} color="black" />
         <Text>Select From Library</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.card}>
+      <TouchableOpacity onPress={handleCamera} style={styles.card}>
         <MaterialIcons name="camera-alt" size={50} color="black" />
         <Text>Open Camera</Text>
       </TouchableOpacity>
