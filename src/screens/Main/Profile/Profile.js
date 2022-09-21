f5ba26b68fcf6370f0094d74f0295bd1302dce31
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-21 23:35:42 +0300

    created base64 hook

--- src/screens/Main/Profile/Profile.js
+++ src/screens/Main/Profile/Profile.js
@@ -13,10 +13,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { logOut } from "../../../redux/userSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as ImagePicker from "expo-image-picker";
-import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
-import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
-import { db, storage } from "../../../../config";
-import uuid from "react-native-uuid";
+
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../../../config";
+import uploadImageAsync from "../../../hooks/uploadImageAsync";
+
 const Profile = () => {
   const navigation = useNavigation();
   const { userInfo } = useSelector((state) => state.user);
@@ -56,31 +57,6 @@ const Profile = () => {
       profilUri: photoURL,
     });
   };
-
-  // base64 tipine çevirme
-  async function uploadImageAsync(uri) {
-    // Why are we using XMLHttpRequest? See:
-    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
-    const blob = await new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        resolve(xhr.response);
-      };
-      xhr.onerror = function (e) {
-        console.log(e);
-        reject(new TypeError("Network request failed"));
-      };
-      xhr.responseType = "blob";
-      xhr.open("GET", uri, true);
-      xhr.send(null);
-    });
-    console.log(blob);
-    const fileRef = ref(storage, uuid.v4());
-    const result = await uploadBytes(fileRef, blob);
-    // We're done with the blob, close and release it
-    // blob.close();
-    return await getDownloadURL(fileRef);
-  }
 
   return (
     <SafeAreaView style={styles.profileContainer}>
