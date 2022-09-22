37189ad87c49a994e49f6500f38005fd544330bb
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-22 05:32:14 +0300

    marker added to map, current location taken and permissions granted

--- src/screens/Main/Map/Map.js
+++ src/screens/Main/Map/Map.js
@@ -1,19 +1,40 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
-import MapView from "react-native-maps";
+import { View, Text, SafeAreaView, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
 import styles from "./Map.style";
+import * as Location from "expo-location";
 export default function Map() {
+  // haritamızın bulunduğu sayfa.
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    currentLocation();
+  }, []);
+
+  const currentLocation = async () => {
+    // Uygulama ilk render edildiğinde haritayı kullanabilmek için izin isteriz.
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("", "Permission to access location was denied");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
   return (
     <View>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <MapView style={styles.map}>
+        {location ? (
+          <Marker
+            //marker kullanarak user profil fotoğrafını harita üzerinde gösteriyoruz.
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        ) : null}
+      </MapView>
     </View>
   );
 }
